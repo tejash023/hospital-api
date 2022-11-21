98bf8fc3c926601d2b649a3ce6d7cf4bb7f4effa
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-21 22:23:17 +0530

    added routes

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || '8080';
 const app = express();
 const db = require('./config/mongoose');
 
+
+//loading the router
+app.get('/', require('./routes/index'));
+
 //starting the server
 app.listen(port, function(err){
   if(err){
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+console.log('Router Loaded');
+
+router.use('/doctor', require('./doctor'));
+
+router.use('/patient', require('./patient'));
+
+router.unsubscribe('/reports', require('./reports'));
+
+module.exports = router;
