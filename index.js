8f61d99d2cd32ee96ce3c2867dc07e2055f05433
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-24 15:28:00 +0530

    added swagger docs

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || '8080';
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 const db = require('./config/mongoose');
 const passport = require('passport');
 const passportJWT = require('./config/passport-jwt');
 
 app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hospital API</h1><a href="/api-docs">Documentation</a>')
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 //loading the router
 app.use('/', require('./routes/index'));
