e835a1c9bb34177ee7f3826964f333f453537719
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-24 18:03:06 +0530

    swagger docs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const passportJWT = require('./config/passport-jwt');
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hospital API</h1><a href="/api-docs">Documentation</a>')
+  res.send('<div style = "font-family: Arial, Helvetica, sans-serif;display: flex; flex-direction:column; gap:2rem;align-items:center; justify-content:center; color:orangered; padding: 30px"><h1 style="font-size: 3rem">🏥 Hospital API 🚑</h1><a href="/api-docs" style = "background: orangered; padding: 10px 20px; color:white; text-decoration:none; font-size:1rem; cursor:pointer; border-radius:5px" href="/api-docs"><p style="font-size: 2rem; margin:0">Documentation 📄</p></div>')
 });
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
